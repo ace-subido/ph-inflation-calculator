bee130f3bf3b99cfa40f8090111b1ceee93d9b59
Ace Subido <ace.subido@gmail.com>
2018-08-27 18:31:37 +0800

    Remove reference of og_image in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,6 @@ import Calculator from "./Calculator"
 import Notes from "./Notes"
 import Header from "./Header"
 
-import { SITE_URL } from "../constants/LinkConstants"
-
-import ogImage from "../assets/og_image.png"
-
 const App = () => {
   return(
     <div className="App">
